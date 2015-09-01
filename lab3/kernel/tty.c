87dcb891bc3ebfa5ac1aa346cc6dca2c85b5dc3c
LuGaXu <329088775@qq.com>
2015-09-01 23:11:15 +0800

    lab3

---
+++ lab3/kernel/tty.c
@@ -0,0 +1,180 @@
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                               tty.c
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                                    Forrest Yu, 2005
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+#include "type.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "keyboard.h"
+#include "proto.h"
+
+#define TTY_FIRST	(tty_table)
+#define TTY_END		(tty_table + NR_CONSOLES)
+
+PRIVATE void init_tty(TTY* p_tty);
+PRIVATE void tty_do_read(TTY* p_tty);
+PRIVATE void tty_do_write(TTY* p_tty);
+PRIVATE void put_key(TTY* p_tty, u32 key);
+
+/*======================================================================*
+                           task_tty
+ *======================================================================*/
+PUBLIC void task_tty()
+{
+	TTY*	p_tty;
+
+	init_keyboard();
+
+	for (p_tty=TTY_FIRST;p_tty<TTY_END;p_tty++) {
+		init_tty(p_tty);
+	}
+	select_console(0);
+	while (1) {
+		for (p_tty=TTY_FIRST;p_tty<TTY_END;p_tty++) {
+			tty_do_read(p_tty);
+			tty_do_write(p_tty);
+		}
+	}
+}
+
+/*======================================================================*
+			   init_tty
+ *======================================================================*/
+PRIVATE void init_tty(TTY* p_tty)
+{
+	p_tty->inbuf_count = 0;
+	p_tty->p_inbuf_head = p_tty->p_inbuf_tail = p_tty->in_buf;
+        /*
+             u32 in_buf[TTY_IN_BYTES];    TTY 输入缓冲区
+             u32* p_inbuf_head;   指向缓冲区中下一个空闲位置
+  	     u32* p_inbuf_tail;   指向键盘任务应处理的键值 
+	     int inbuf_count;    缓冲区中已经填充了多少  
+        */
+
+	init_screen(p_tty);
+}
+
+/*======================================================================*
+				in_process
+ *======================================================================*/
+PUBLIC void in_process(TTY* p_tty, u32 key)
+{
+        char output[2] = {'\0', '\0'};
+
+        if (!(key & FLAG_EXT)) {
+		put_key(p_tty, key);
+        }
+        else {
+                int raw_code = key & MASK_RAW;
+                switch(raw_code) {
+                case ENTER:
+			put_key(p_tty, '\n');
+			break;
+                case BACKSPACE:
+			put_key(p_tty, '\b');
+			break;
+                case TAB:
+                        put_key(p_tty,'\t');
+                        //put_key(p_tty,' ');
+                        //put_key(p_tty,' ');
+                        //put_key(p_tty,' ');
+                        //set_Tab();
+                        //put_key(p_tty,' ');
+                        break;
+                case UP:
+                        if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
+				scroll_screen(p_tty->p_console, SCR_DN);
+                        }
+			break;
+		case DOWN:
+			if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
+				scroll_screen(p_tty->p_console, SCR_UP);
+			}
+			break;
+               //change color
+		case F1:
+                        set_color(1);
+                        break;
+		case F2:
+                        set_color(2);
+                        break;
+		case F3:
+                        set_color(3);
+                        break;
+		case F4:
+                        set_color(4);
+                        break;
+		case F5:
+                        set_color(5);
+                        break;
+		case F6:
+		case F7:
+		case F8:
+		case F9:
+		case F10:
+		case F11:
+		case F12:
+			/* Alt + F1~F12 */
+			if ((key & FLAG_ALT_L) || (key & FLAG_ALT_R)) {
+				select_console(raw_code - F1);
+			}
+			break;
+                default:
+                        break;
+                }
+        }
+}
+
+/*======================================================================*
+			      put_key
+*======================================================================*/
+PRIVATE void put_key(TTY* p_tty, u32 key)
+{
+	if (p_tty->inbuf_count < TTY_IN_BYTES) {
+		*(p_tty->p_inbuf_head) = key;
+		p_tty->p_inbuf_head++;
+		if (p_tty->p_inbuf_head == p_tty->in_buf + TTY_IN_BYTES) {
+			p_tty->p_inbuf_head = p_tty->in_buf;
+		}
+		p_tty->inbuf_count++;
+	}
+}
+
+
+/*======================================================================*
+			      tty_do_read
+ *======================================================================*/
+PRIVATE void tty_do_read(TTY* p_tty)
+{
+	if (is_current_console(p_tty->p_console)) {
+		keyboard_read(p_tty);
+	}
+}
+
+
+/*======================================================================*
+			      tty_do_write
+ *======================================================================*/
+PRIVATE void tty_do_write(TTY* p_tty)
+{
+	if (p_tty->inbuf_count) {
+		char ch = *(p_tty->p_inbuf_tail);
+		p_tty->p_inbuf_tail++;
+		if (p_tty->p_inbuf_tail == p_tty->in_buf + TTY_IN_BYTES) {
+			p_tty->p_inbuf_tail = p_tty->in_buf;
+		}
+		p_tty->inbuf_count--;
+
+		out_char(p_tty->p_console, ch);
+	}
+}
+
+
